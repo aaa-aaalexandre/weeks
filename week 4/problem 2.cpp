b5dcf577c6e842cdd91a506c6d61fc4fa3aa3821
aaa-aaalexandre <alexanderariadana@gmail.com>
2024-11-01 13:24:21 +0700

    Update problem 2.cpp

--- week 4/problem 2.cpp
+++ week 4/problem 2.cpp
@@ -4,9 +4,8 @@ using namespace std;
 
 int main() {
     int jeff;
-    long sum = 0;
 
-    cout << "Enter an integer: ";
+    cout << "Enter a number: ";
     cin >> jeff;
     for(int i = 1; i <= 10; i++) {
         cout << "The number multiplied by " << i << " is " << jeff * i<<std::endl; ;
