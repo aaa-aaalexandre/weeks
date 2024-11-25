defd15202525e1f313db3514eed59b1ed934d4c5
aaa-aaalexandre <alexanderariadana@gmail.com>
2024-11-25 16:00:57 +0700

    Create problem 6 testing.cpp

---
+++ responsi/problem 6 testing.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+#include <cmath>
+#include <iospace>
+int main() {
+	int a;
+  long fact == 1;
+	cout << "Enter a number" << endl;
+	cin >> a;
+  for (i; fact <= a; i++)
+       int fact *= fact;
+  cout << "The factorial of the number you just submitted is" << a << "!" << endl;
+
+  return 0;
+}
